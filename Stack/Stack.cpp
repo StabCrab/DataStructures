4afa5e8c1a6a419dda434d96ab26b0094219f5d9
StabCrab <37272657+StabCrab@users.noreply.github.com>
2020-04-28 01:21:39 +0300

    Update Stack.cpp

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,3 +1,7 @@
+//
+// Created by trykr on 25.04.2020.
+//
+
 #include "Stack.h"
 #include "ListStack.h"
 #include "VectorStack.h"
@@ -6,70 +10,96 @@
 #include <stdexcept>
 
 Stack::Stack(StackContainer container)
-	: _containerType(container)
+        : _containerType(container)
 {
-	switch (container)
-	{
-	case StackContainer::List: {
-		_pimpl = new ListStack();	// конкретизируйте под ваши конструкторы, если надо
-		break;
-	}
-	case StackContainer::Vector: {
-		_pimpl = new VectorStack();	// конкретизируйте под ваши конструкторы, если надо
-		break;
-	}
-	default:
-		throw std::runtime_error("Неизвестный тип контейнера");
-	}
+    switch (container)
+    {
+        case StackContainer::List: {
+            _pimpl = new ListStack();	// конкретизируйте под ваши конструкторы, если надо
+            break;
+        }
+        case StackContainer::Vector: {
+            _pimpl = new VectorStack();	// конкретизируйте под ваши конструкторы, если надо
+            break;
+        }
+        default:
+            throw std::runtime_error("Неизвестный тип контейнера");
+    }
 }
 
 Stack::Stack(const ValueType* valueArray, const size_t arraySize, StackContainer container)
 {
-	// принцип тот же, что и в прошлом конструкторе
+    switch (container)
+    {
+        case StackContainer::List: {
+            _pimpl = new ListStack();	// конкретизируйте под ваши конструкторы, если надо
+            break;
+        }
+        case StackContainer::Vector: {
+            _pimpl = new VectorStack(valueArray, arraySize);	// конкретизируйте под ваши конструкторы, если надо
+            break;
+        }
+        default:
+            throw std::runtime_error("Неизвестный тип контейнера");
+    }// принцип тот же, что и в прошлом конструкторе
 }
 
 Stack::Stack(const Stack& copyStack)
 {
-	// сами
+   *_pimpl = *(copyStack._pimpl);
 }
 
-Stack& Stack::operator=(const Stack& copyStack)
+Stack& Stack::operator = (const Stack& copyStack)
 {
-	// TODO: вставьте здесь оператор return
+    if (this == &copyStack)
+        return *this;
+    this->_containerType = copyStack._containerType;
+    delete this->_pimpl;
+    switch (this->_containerType)
+    {
+        case StackContainer::List: {
+            _pimpl = new ListStack(
+                    *(dynamic_cast<ListStack *>(copyStack._pimpl)));    // конкретизируйте под ваши конструкторы, если надо
+            break;
+        }
+        case StackContainer::Vector: {
+            _pimpl = new VectorStack(
+                    *(dynamic_cast<VectorStack *>(copyStack._pimpl)));    // конкретизируйте под ваши конструкторы, если надо
+            break;
+        }
+        default:
+            throw std::runtime_error("Неизвестный тип контейнера");
+    }
+    return *this;
 }
 
 Stack::~Stack()
 {
-	delete _pimpl;		// композиция!
+    delete _pimpl;		// композиция!
 }
 
 void Stack::push(const ValueType& value)
 {
-	// можно, т.к. push определен в интерфейсе
-	_pimpl->push(value);
+    // можно, т.к. push определен в интерфейсе
+    _pimpl->push(value);
 }
 
 void Stack::pop()
 {
-	_pimpl->pop();
-}
-
-ValueType& Stack::top()
-{
-	return _pimpl->top();
+    _pimpl->pop();
 }
 
 const ValueType& Stack::top() const
 {
-	return _pimpl->top();
+    return this->_pimpl->top();
 }
 
 bool Stack::isEmpty() const
 {
-	return _pimpl->isEmpty();
+    return _pimpl->isEmpty();
 }
 
 size_t Stack::size() const
 {
-	return _pimpl->isEmpty();
+    return _pimpl->size();
 }
